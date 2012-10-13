937d626c84be1a84262f6bbbab32e068e3f276d8
Joey Schoblaska <joey.schoblaska@gmail.com>
2012-10-13 16:43:35 -0500

    month bar offsets

--- inseason.rb
+++ inseason.rb
@@ -1,4 +1,13 @@
 class InSeason < Sinatra::Base
+  helpers do
+    def month_bars
+      width = 703
+      offset = -(width/365.0 * (Time.now.yday - 1) - width/2).to_i
+      "<img class='month-bars' src='images/month-bars.png' style='left: #{offset}px'>" +
+      "<img class='month-bars' src='images/month-bars.png' style='left: #{offset + width}px'>"
+    end
+  end
+
   get '/' do
     haml :index, :format => :html5
   end
