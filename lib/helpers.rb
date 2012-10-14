db65cc403eaaf4b652ef5fbfefcaea4667242f26
Joey Schoblaska <joey.schoblaska@gmail.com>
2012-10-14 18:29:16 -0500

    shrunk season bars and centered them on page

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,7 +1,7 @@
 class InSeason
   module Helpers
     def month_bars
-      width = 703
+      width = 500
       offset = -(width/365.0 * (Time.now.yday - 1) - width/2).to_i
       "<img class='month-bars' src='images/month-bars.png' style='left: #{offset}px'>" +
       "<img class='month-bars' src='images/month-bars.png' style='left: #{offset + width}px'>"
