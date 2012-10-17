ed74d5060cc36cd3478f4dafea5ad321aabe9fed
Joey Schoblaska <joey.schoblaska@gmail.com>
2012-10-16 21:32:50 -0500

    more of a line, really

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,6 +1,6 @@
 class InSeason
   module Helpers
-    def current_pixel
+    def current_line
       offset = (Time.now.yday / 365.0 * 500).to_i + 1
       offset -= 1 if Date.today.leap? && offset > 60
       "<img src='images/current-pixel.png' class='current-line' style='left: #{offset}px'>"
