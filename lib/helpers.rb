dd1d414db9261b89b754c09b4a0492131fb78967
Joey Schoblaska <joey.schoblaska@gmail.com>
2012-10-21 21:11:03 -0500

    rough pass at rendering actual crops

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -5,5 +5,11 @@ class InSeason
       offset -= 1 if Date.today.leap? && offset > 60
       "<img src='images/current-pixel.png' class='current-line' style='left: #{offset}px'>"
     end
+
+    def season_bars(seasons)
+      padding = ((seasons.first[0]) / 365.0 * 500).to_i
+      width = ((seasons.first[1] + 1) / 365.0 * 500).to_i
+      "<div class='season-bar-inner' style='width: #{width}px; margin-left: #{padding}px'></div>"
+    end
   end
 end
