df94061dd1f5f02e8d8dc18e9284b07b1ba3cfd1
Joey Schoblaska <joey.schoblaska@gmail.com>
2012-11-02 15:15:33 -0500

    handle display of multiple season bars

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -6,9 +6,19 @@ class InSeason
     end
 
     def season_bars(seasons)
-      padding = ((seasons.first[0]) / 365.0 * 500).to_i
-      width = ((seasons.first[1] + 1) / 365.0 * 500).to_i
-      "<div class='season-bar-inner' style='width: #{width}px; margin-left: #{padding}px'></div>"
+      seasons = seasons.sort_by{|s| s[0]}
+      seasons.map do |season|
+        if season == seasons.first
+          padding = (season[0] / 365.0 * 500).to_i
+        else
+          prev_season = seasons[seasons.index(season) - 1]
+          padding = ((season[0] - (prev_season[0] + prev_season[1])) / 365.0 * 500).to_i
+        end
+
+        width = (season[1] / 365.0 * 500).to_i
+
+        "<div class='season-bar-inner' style='width: #{width}px; margin-left: #{padding}px'></div>"
+      end.join
     end
   end
 end
